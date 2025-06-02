d02b2e62e203d71f8cfc00ef84229548366756d3
stellaraj <rajakirthicc@gmail.com>
2025-06-02 21:33:45 +0530

    .

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6497757690').split()]
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5050588448 730412993').split()]
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'codeflix_bots').split(',')
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002037205021"))
     DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002037205021"))
